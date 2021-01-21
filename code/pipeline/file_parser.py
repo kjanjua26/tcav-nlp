ba2583e1af9a29f002774363faae914bd24c9636
Kamran Janjua <kamranejaz98@gmail.com>
2021-01-21 21:36:21 +0300

    Minor change

--- code/pipeline/file_parser.py
+++ code/pipeline/file_parser.py
@@ -6,15 +6,14 @@ Parse professions with [MASK] in a single txt file.
 from tqdm import tqdm
 import re
 
-output = "/Users/Janjua/Desktop/QCRI/Work/aux_classifier/aux_classifier/mask.txt"
-
 def reformat():
     """Read the file and convert [his, her] to MASK."""
     
     to_mask = ['his', 'her']
     outlines = []
-    fp = "/Users/Janjua/Desktop/QCRI/Work/aux_classifier/aux_classifier/handcrafted.ende.txt"
-
+    fp = "handcrafted.ende.txt"
+    output = "mask.txt"
+    
     with open(fp, 'r') as data:
         lines = data.readlines()
         for line in tqdm(lines):
