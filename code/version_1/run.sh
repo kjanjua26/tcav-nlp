7ce8279c534c85c7086d440e49ea6325aa10198b
Kamran Janjua <kamranejaz98@gmail.com>
2021-01-31 21:24:17 +0300

    Minor

--- code/version_1/run.sh
+++ code/version_1/run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-concept_path="../../../data/dconcept.in"
-concept_labels="../../../data/dconcept_labels.in"
-concept_activations="../../../data/dconcept_acts.json"
+base_folder="paper_test"
+concept_path="../../../${base_folder}/dconcept.in"
+concept_labels="../../../${base_folder}/dconcept_labels.in"
+concept_activations="../../../${base_folder}/dconcept_acts.json"
 
-base_path="../../../data/dbase.txt"
-base_labels="../../../data/dbase_labels"
-base_activations="../../../data/dbase_activations.json"
+base_path="../../../${base_folder}/dbase.txt"
+base_labels="../../../${base_folder}/dbase_labels.txt"
+base_activations="../../../${base_folder}/dbase_activations.json"
 
-output_directory="../../../data/results"
+output_directory="../../../${base_folder}/results"
 
-layer_wise_cav_pickle_path="../../../data/results/layer_wise_cavs.pickle"
-layer_wise_cav_random_path="../../../data/results/layer_wise_random_cavs.pickle"
-mode_tcav="w"
+layer_wise_cav_pickle_path="../../../${base_folder}/results/layer_wise_cavs.pickle"
+layer_wise_cav_random_path="../../../${base_folder}/results/layer_wise_random_cavs.pickle"
+mode_tcav="wm"
 word="[MASK]"
 
 echo "Extract Activations!"
@@ -23,13 +24,13 @@ echo "Prepare Concepts and Training CAVs!"
 python prepare_concepts.py -i $concept_labels -l $concept_labels -e $concept_activations -c "DT NNS CC JJ" -o $output_directory
 
 echo "Making Directories to Store Results!"
-mkdir ../../../data/results/layer_wise
-mkdir ../../../data/results/concept_wise
+mkdir ../../../${base_folder}/results/layer_wise
+mkdir ../../../${base_folder}/results/concept_wise
 
 echo "Computing TCAVs!"
-python compute_tcavs.py -b $base_activations -c $layer_wise_cav_pickle_path -r $layer_wise_cav_random_path -o $output_directory -bs $base_path -m $mode_tcav -w $word
+python compute_tcavs.py -b $base_activations -c $layer_wise_cav_pickle_path -r $layer_wise_cav_random_path -o $output_directory -bs $base_path -bl $base_labels -m $mode_tcav -w $word
+
 
 # Other Mode Option:
-# python compute_tcavs.py -b "../../../data/dbase_activations.json" -c "../../../data/results/layer_wise_cavs.pickle" -r "../../../data/results/layer_wise_random_cavs.pickle" -o "../../../data/results" -bs "../../../data/dbase.txt" -bl "../../../data/dbase_labels.txt" -m "wm" -w "[MASK]"
+#python compute_tcavs.py -b $base_activations -c $layer_wise_cav_pickle_path -r $layer_wise_cav_random_path -o $output_directory -bs $base_path -m $mode_tcav -w $word
 #python3.7 -m streamlit
-
